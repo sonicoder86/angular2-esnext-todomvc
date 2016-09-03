af908530cad17b74150e9937e30598dbcdd6f163
blacksonic <soos.gabor86@gmail.com>
2016-09-03 19:20:48 +0200

    remove unnecessary aliases

--- webpack.config.js
+++ webpack.config.js
@@ -18,18 +18,5 @@ module.exports = {
     ]
   },
 
-  resolve: {
-    alias: {
-      '@angular/common': '@angular/common/bundles/common.umd.min.js',
-      '@angular/core': '@angular/core/bundles/core.umd.min.js',
-      '@angular/router': '@angular/router/bundles/router.umd.min.js',
-      '@angular/forms': '@angular/forms/bundles/forms.umd.min.js',
-      '@angular/http': '@angular/http/bundles/http.umd.min.js',
-      '@angular/compiler': '@angular/compiler/bundles/compiler.umd.min.js',
-      '@angular/platform-browser': '@angular/platform-browser/bundles/platform-browser.umd.min.js',
-      '@angular/platform-browser-dynamic': '@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.min.js'
-    }
-  },
-
   devtool: 'source-map'
 };
