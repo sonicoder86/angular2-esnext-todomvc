222d4b051f22dde05966d6bb52e53ff78a0b66de
blacksonic <soos.gabor86@gmail.com>
2016-09-22 20:54:39 +0200

    move all angular libraries to vendor

--- client/app/vendor.js
+++ client/app/vendor.js
@@ -5,7 +5,10 @@ import 'localStorage';
 import 'node-uuid';
 
 import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
 import '@angular/forms';
 import '@angular/http';
 import '@angular/router';
 import '@angular/core';
+import '@angular/common';
+import '@angular/compiler';
